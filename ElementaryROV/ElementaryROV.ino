bdecdff2dfb436529d93aa79c58fe016c18c496b
zinjkov <zinjkov.su@gmail.com>
2020-02-11 12:55:11 +1000

    change max motor speed

--- ElementaryROV/ElementaryROV.ino
+++ ElementaryROV/ElementaryROV.ino
@@ -79,7 +79,7 @@ int getAxisZVal() {
 	return axis;
 }
 
-const int MAXPOWER = 70;
+const int MAXPOWER = 100;
 
 int regulator(int pow) {
 	return constrain(pow, -MAXPOWER / getPowerDelimetr(), MAXPOWER / getPowerDelimetr());
